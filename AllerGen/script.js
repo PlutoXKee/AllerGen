b80b93b96a0c37ede4858793579c8b8ad92513d1
Chillayy <shemar.pradia@gmail.com>
2023-08-07 15:39:53 -0500

    comitted 8/7/2023

--- AllerGen/script.js
+++ AllerGen/script.js
@@ -1,2 +1,19 @@
 let prefs = [];
 let diet = [];
+let favs = [];
+
+let hamburgerMenu = document.getElementById("hamburger-menu");
+let hamburgerSubMenu = document.getElementById("hamburger-submenu");
+let favIcon = document.getElementsByClassName("fav-container");
+
+let hamburgerSubMenuState = 1;
+
+hamburgerMenu.onclick = function () {
+  if (hamburgerSubMenuState == 1) {
+    hamburgerSubMenuState = 2;
+    hamburgerSubMenu.style.display = "none";
+  } else {
+    hamburgerSubMenuState = 1;
+    hamburgerSubMenu.style.display = "flex";
+  }
+};
